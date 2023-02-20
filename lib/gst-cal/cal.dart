1e61cf50ae32b29fa311c4be1005dea471351409
Vivek <tailorvivekgec19@gmail.com>
2023-02-20 13:08:37 +0530

    gt

--- lib/gst-cal/cal.dart
+++ lib/gst-cal/cal.dart
@@ -10,14 +10,12 @@ class Gst extends StatefulWidget {
 class _GstState extends State<Gst> {
   TextEditingController txtno=TextEditingController();
   TextEditingController txtta=TextEditingController();
-  int a=0;
-  int d=0;
+  double a=1;
+  double d=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           // enter the no
           TextField(
@@ -34,8 +32,11 @@ class _GstState extends State<Gst> {
           ),
           ElevatedButton(onPressed: () {
             String birth=txtno.text;
-            int a=int.parse(birth);
-
+            print(birth);
+            double a=double.parse(birth);
+            setState(() {
+              a = a * 0.03;
+            });
           }, child: Text("3%")),
           Container(
             height: 20,
@@ -43,7 +44,12 @@ class _GstState extends State<Gst> {
             //child: Text("5%"),
           ),
           ElevatedButton(onPressed: () {
-
+            String birth=txtno.text;
+            print(birth);
+            double a=double.parse(birth);
+            setState(() {
+              a = a * 0.05;
+            });
           }, child: Text('5%')),
           Container(
             height: 20,
@@ -64,134 +70,143 @@ class _GstState extends State<Gst> {
       ),
           ElevatedButton(onPressed: () {
            setState(() {
-             int d=a/100;
+             setState(() {
+               print("$a");
+               a= a * 0.05;
+             });
+             print("$a");
            });
           }, child: Text("sumil")),
           // final peice
-          Row(
-            children: [
-              Container(
-                height: 50,
-                width: 60,
-                child: Text("F"),
-              ),
-              Container(
-                height: 50,
-                width: 100,
-                child: Text("Final price"),
-              ),
-            ],
-          ),
+          // Row(
+          //   children: [
+          //     Container(
+          //       height: 50,
+          //       width: 60,
+          //       child: Text("F"),
+          //     ),
+          //     Container(
+          //       height: 50,
+          //       width: 100,
+          //       child: Text("Final price"),
+          //     ),
+          //   ],
+          // ),
 
+          Text("$a"),
           SizedBox(height: 20),
-          //no 1 to 9
-          Container(
-            height: 300,
-            width: 200,
-            child: Column(
-              children: [
-                Row(
-                children: [
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('7'),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('8'),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('9'),
-                  ),
-                  Container(
-                    height: 35,
-                    width: 20,
-                    color: Color(0xffFF6B00),
-                    child: Text('Ac'),
-                  ),
-                ],
-              ),
-              Row(children: [
-                Container(
-                  height: 20,
-                  width: 20,
-                  color: Color(000252427),
-                  child: Text('4'),
-                ),
-                Container(
-                  height: 20,
-                  width: 20,
-                  color: Color(000252427),
-                  child: Text('5'),
-                ),
-                Container(
-                  height: 20,
-                  width: 20,
-                  color: Color(000252427),
-                  child: Text('6'),
-                ),
-                Container(
-                  height: 35,
-                  width: 20,
-                  
-                  decoration: BoxDecoration(
-                      color: Color(0xffFF6B00),
-                  ),
-                  child: Text('Ac'),
-                ),
-              ],),
-              Row(children: [
-        Container(
-          height: 20,
-          width: 20,
-          color: Color(000252427),
-          child: Text('1'),
-        ),
-        Container(
-          height: 20,
-          width: 20,
-          color: Color(000252427),
-          child: Text('2'),
-        ),
-        Container(
-          height: 20,
-          width: 20,
-          color: Color(000252427),
-          child: Text('3'),
-        ),
-      ],),
-                Row(children: [
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('00'),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('0'),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    color: Color(000252427),
-                    child: Text('.'),
-                  ),
-                ],)
-              ],
-            ),
-          ),
         ],
       ),
     );
   }
+
+
+
 }
+
+//no 1 to 9
+//     Container(
+//       height: 300,
+//       width: 200,
+//       child: Column(
+//         children: [
+//           Row(
+//           children: [
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('7'),
+//             ),
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('8'),
+//             ),
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('9'),
+//             ),
+//             Container(
+//               height: 35,
+//               width: 20,
+//               color: Color(0xffFF6B00),
+//               child: Text('Ac'),
+//             ),
+//           ],
+//         ),
+//         Row(children: [
+//           Container(
+//             height: 20,
+//             width: 20,
+//             color: Color(000252427),
+//             child: Text('4'),
+//           ),
+//           Container(
+//             height: 20,
+//             width: 20,
+//             color: Color(000252427),
+//             child: Text('5'),
+//           ),
+//           Container(
+//             height: 20,
+//             width: 20,
+//             color: Color(000252427),
+//             child: Text('6'),
+//           ),
+//           Container(
+//             height: 35,
+//             width: 20,
+//
+//             decoration: BoxDecoration(
+//                 color: Color(0xffFF6B00),
+//             ),
+//             child: Text('Ac'),
+//           ),
+//         ],),
+//         Row(children: [
+//   Container(
+//     height: 20,
+//     width: 20,
+//     color: Color(000252427),
+//     child: Text('1'),
+//   ),
+//   Container(
+//     height: 20,
+//     width: 20,
+//     color: Color(000252427),
+//     child: Text('2'),
+//   ),
+//   Container(
+//     height: 20,
+//     width: 20,
+//     color: Color(000252427),
+//     child: Text('3'),
+//   ),
+// ],),
+//           Row(children: [
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('00'),
+//             ),
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('0'),
+//             ),
+//             Container(
+//               height: 20,
+//               width: 20,
+//               color: Color(000252427),
+//               child: Text('.'),
+//             ),
+//           ],)
+//         ],
+//       ),
+//     ),
